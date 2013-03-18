7479c7a0c3fbbb527756348b99cfb1f192ef433d
Christoph Wiechert <wio@psitrax.de>
2013-03-18 16:11:23 +0100

    Added: exit-statement if GlobalContentelements is used with Contao3

--- config/config.php
+++ config/config.php
@@ -9,6 +9,28 @@
  * @licence LGPL
  */
 
+if(version_compare(VERSION,'3.0','>='))
+{
+header('Content-Type: text/plain');
+echo <<<EOERR
+GlobalContentelements is NOT compatible with Contao 3!!!
+
+Please delete system/modules/GlobalContentelements folder!
+To migrate the Database you have to run some queries:
+
+### if you use news4ward:
+UPDATE tl_content SET ptable='tl_news4ward_article' WHERE do='news4ward';
+
+### if you use boxes4ward:
+UPDATE tl_content SET ptable='tl_boxes4ward_article' WHERE do='boxes4ward';
+
+if the ptable row dosnt already exists, create it with
+ALTER TABLE tl_content ADD ptable VARCHAR(64) NOT NULL default '';
+EOERR;
+
+exit;
+}
+
 // Register Callback to prevent deleting the wrong CEs
 $GLOBALS['TL_HOOKS']['reviseTable'][] = array('GlobalContentelements','reviseTable');
 
